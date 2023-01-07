83366104b6a72a2d815a6cb271e5bb3ad7a8df67
Jarkko Skyttälä <jarkko92@gmail.com>
2023-01-07 20:36:25 +0200

    Refactor day 3 again

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,46 +1,33 @@
 use std::fs;
 
-#[derive(Debug)]
-struct Matrix {
-    value: Vec<Vec<char>>,
+fn get_char_at(
+    matrix: &Vec<Vec<char>>,
+    vertical_index: usize,
+    horizontal_index: usize,
+) -> Option<&char> {
+    let horizontal_row = matrix.get(vertical_index)?;
+
+    let horizontal_row_length = horizontal_row.len();
+    let overlap_count = horizontal_index / horizontal_row_length;
+
+    horizontal_row.get(horizontal_index - horizontal_row_length * overlap_count)
 }
 
-impl Matrix {
-    fn get(&self, vertical_index: usize, horizontal_index: usize) -> Option<char> {
-        let horizontal_row = &self.value.get(vertical_index)?;
+fn travel(matrix: &Vec<Vec<char>>, right_amount: usize, down_amount: usize) -> i32 {
+    let mut tree_counter = 0;
+    let mut start_horizontal_index = 0;
 
-        let horizontal_row_length = horizontal_row.len();
+    for vertical in 1..matrix.iter().count() {
+        start_horizontal_index += right_amount;
 
-        if horizontal_index > (horizontal_row_length - 1) {
-            let overlap_count = horizontal_index / horizontal_row_length;
-            let new_horizontal_index = horizontal_index - horizontal_row_length * overlap_count;
-
-            return horizontal_row.get(new_horizontal_index).copied();
-        }
-
-        horizontal_row.get(horizontal_index).copied()
-    }
-
-    fn horizontal_rows_count(&self) -> usize {
-        self.value.iter().count()
-    }
-
-    fn travel(&self, right_amount: usize, down_amount: usize) -> i32 {
-        let mut tree_counter = 0;
-        let mut start_horizontal_index = 0;
-
-        for vertical in 1..self.horizontal_rows_count() {
-            start_horizontal_index += right_amount;
-
-            if let Some(ch) = self.get(vertical * down_amount, start_horizontal_index) {
-                if ch == '#' {
-                    tree_counter += 1;
-                }
+        if let Some(&ch) = get_char_at(matrix, vertical * down_amount, start_horizontal_index) {
+            if ch == '#' {
+                tree_counter += 1;
             }
         }
-
-        tree_counter
     }
+
+    tree_counter
 }
 
 pub fn run() {
@@ -48,24 +35,22 @@ pub fn run() {
 
     let file_path = "src/inputs/day3.txt";
 
-    let matrix = Matrix {
-        value: fs::read_to_string(file_path)
-            .expect("Should have been able to read the file")
-            .lines()
-            .map(|x| x.chars().collect())
-            .collect(),
-    };
+    let matrix: Vec<Vec<char>> = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file")
+        .lines()
+        .map(|x| x.chars().collect())
+        .collect();
 
     //225
-    println!("Part 1 answer is: {}", matrix.travel(3, 1));
+    println!("Part 1 answer is: {}", travel(&matrix, 3, 1));
 
     // 1115775000
     println!(
         "Part 2 answer is: {}",
-        matrix.travel(1, 1)
-            * matrix.travel(3, 1)
-            * matrix.travel(5, 1)
-            * matrix.travel(7, 1)
-            * matrix.travel(1, 2)
+        travel(&matrix, 1, 1)
+            * travel(&matrix, 3, 1)
+            * travel(&matrix, 5, 1)
+            * travel(&matrix, 7, 1)
+            * travel(&matrix, 1, 2)
     );
 }
